24a1919c02065490950715f15108b075f1208ea3
tabetrahan <trahantet@gmail.com>
2024-02-29 12:27:54 -0500

    universal and translatable

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -128,7 +128,7 @@ function MyApp({ Component, pageProps }) {
     <main
       className={`${yeseva_one.variable} ${inter.variable} ${sailors.variable}  ${quicksand.variable}`}
     >
-      <Layout config={pageProps.config} locales={pageProps.locales} locale={pageProps.locale} defaultLocale={pageProps.defaultLocale}>
+      <Layout config={pageProps.config.content} locales={pageProps.locales} locale={pageProps.locale} defaultLocale={pageProps.defaultLocale}>
         <Component {...pageProps} />
       </Layout>
     </main>
