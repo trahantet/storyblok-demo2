e847214095c4099ce46b11b107cb4db28f1932fa
tabetrahan <trahantet@gmail.com>
2024-03-01 10:57:15 -0500

    trying to figure out grid column flexibility

--- src/components/Config.js
+++ src/components/Config.js
@@ -61,7 +61,7 @@ console.log(blok.header_menu)
               <Link
                 key={route._uid}
                 //  href="https://airtable.com/appovsAD7ZM2tQUFX/shrtFmpmtxoRUEcBU"
-                href={route.link.url ?  route.link.url : route.link.cached_url === "home" ?  `./#${route.link.anchor}` : route.link.cached_url }
+                href={route.link.url ?  route.link.url : route.link.cached_url === "home"| route.link.cached_url === "/ESP/home" ?  `./#${route.link.anchor}` : route.link.cached_url }
                 target={route.link.target}
                 className="text-base font-medium text-gray-500 hover:text-gray-900 "
               >
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,11 +1,11 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
 const Grid = ({ blok }) => {
-  const desktop = blok.desktop_only ? `hidden sm:grid sm:grid-cols-${blok.column_num}` : `grid sm:grid-cols-${blok.column_num}`
+  const desktop = blok.desktop_only ? `hidden sm:grid sm:grid-cols-${blok.column_num}` : `grid`
 
   return (
     <div
-      className={`auto-rows-min gap-x-[4%] gap-y-4 px-${blok.px} sm:px-12 items-stretch ${desktop} `}
+      className={`auto-rows-min gap-x-[4%] gap-y-4 px-${blok.px} sm:px-12 items-stretch ${desktop}  sm:grid-cols-${blok.column_num}`}
       // style={{ gridTemplateColumns: `repeat(${blok.column_num}, 1fr)` }}
       {...storyblokEditable(blok)}
     >
