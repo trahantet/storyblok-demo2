f9547c1c2d7c6451f010734f7bdf624d74d5b5a2
tabetrahan <trahantet@gmail.com>
2024-03-11 14:25:13 -0400

    no quick exit on mobile

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,8 +21,20 @@ const Layout = ({ children, config, locale, locales, defaultLocale, footer }) =>
       <Config blok={config.content} locales={locales} locale={locale} defaultLocale={defaultLocale}/>
        
       {children}
-      <button className="fixed -right-[2.1%] top-[50%] rotate-90 bg-lav p-[1%] uppercase flex align-center" onClick={getAway}>Quick Exit</button>
+      <button className="exit fixed -right-[2.1%] top-[50%] rotate-90 bg-lav p-[1%] uppercase flex align-center" onClick={getAway}>Quick Exit</button>
       <Footer blok={footer.content}/>
+      <style jsx>{`
+        .exit {
+         position:fixed
+        }
+        @media (max-width: 768px) {
+          .exit {
+            
+            display: none;
+            
+          }
+        }
+      `}</style>
     </div>
 
 );
