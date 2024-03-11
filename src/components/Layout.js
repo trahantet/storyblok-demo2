0f53a1fbd3a664cdbcee786e586dd826afeb2ccc
tabetrahan <trahantet@gmail.com>
2024-03-11 14:21:57 -0400

    quick exit button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,16 +2,29 @@ import Navigation from "./Navigation";
 import Footer from "./Footer";
 import Config from "./Config";
 
+
+function getAway() {
+    window.open("http://weather.com", "_newtab"); //Open new tab and switch to it.
+    window.location.replace("http://google.com"); //Change current tab (doesn't work on codepen
+    console.log("clicked")
+  }
+
 const Layout = ({ children, config, locale, locales, defaultLocale, footer }) => (
+  
+  
+  
+
+  
   // console.log(home.content),
-  (
+
     <div>
       <Config blok={config.content} locales={locales} locale={locale} defaultLocale={defaultLocale}/>
        
       {children}
+      <button className="fixed -right-[2.1%] top-[50%] rotate-90 bg-lav p-[1%] uppercase flex align-center" onClick={getAway}>Quick Exit</button>
       <Footer blok={footer.content}/>
     </div>
-  )
+
 );
 
 export default Layout;
